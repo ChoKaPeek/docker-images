2a85afd43d4e23d328a46e54729426bf2b59d7ff
Amael Tardif <amael.tardif@epita.fr>
2020-07-31 20:11:39 +0200

    ci: squash is not implemented in buildx, falling back to second dockerfile

---
+++ ci/squash-runs.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# By ChoKaPeek <amael.tardif@epita.fr>
+# The objective here is to squash docker images. Merging commands saves space.
+# No magic here, this script only merges adjacent RUN commands.
+
+[ "$1" ] || {
+  echo "Usage: ./squash-runs.sh PATH_TO_DOCKERFILE" && exit 1
+}
+
+path=$1/Dockerfile
+out=$1/Dockerfile_squash
+keywords="COPY|ARG|FROM|ENV|WORKDIR|ENTRYPOINT|CMD"
+in_run=0
+
+write() {
+  echo "$1 \\" >> $out
+}
+
+rm -f $out
+
+while IFS=$'\n' read line; do
+  trimmed=$(echo "$line" | awk '{$1=$1};1')
+  if [[ "${trimmed%% *}" =~ ^RUN$ ]]; then
+    if [ $in_run -eq 1 ]; then
+      write " && ${trimmed#* }"
+    else
+      write "$trimmed"
+      in_run=1
+    fi
+  else
+    if [ $in_run -eq 1 ]; then
+      if [[ "${trimmed%% *}" =~ ^($keywords)$ ]]; then
+        echo "" >> $out
+        echo "$trimmed" >> $out
+        in_run=0
+      else
+        ! [[ "$trimmed" =~ ^#.*$ ]] && write "$trimmed"
+      fi
+    else
+      echo "$trimmed" >> $out
+    fi
+  fi
+done < $path
+echo "" >> $out
